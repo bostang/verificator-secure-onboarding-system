993a2fe0b6e892c82c74ca392719c077869aefbd
bostang <bostangsteiitb2020@gmail.com>
2025-07-24 05:07:06 +0700

    REMOVE UNUSED LIBRARY IN SECURITYCONFIG.JAVA

--- src/main/java/com/dukcapil/service/config/SecurityConfig.java
+++ src/main/java/com/dukcapil/service/config/SecurityConfig.java
@@ -11,7 +11,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
-import java.util.List;
+// import java.util.List;
 
 @Configuration
 @EnableWebSecurity
